f8d7d58637bef6402cb20e6372dc9943b595baaa
rohan_bbm <rohan@bedbathmore.com>
2017-02-23 10:55:44 +0530

    minor changes

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,65 +1,42 @@
 $(document).ready(function() {
 
-  // Starting location is San Francisco
-  var startingLocation = {
-    lat: 37.773972,
-    lng: -122.431297
-  };
-
-  /** Map Module **/
-  var Map = (function(initialLocation) {
-    var module = {};
-
-    // Default starting location is San Francisco
-    var startingLocation = initialLocation || {
-      lat: 37.773972,
-      lng: -122.431297
-    };
-
-    // Markers
+  var Map = (function() {
+    var mapModule = {};
     var markers = [];
 
-    // One info window for the map
     var infoWindow = new google.maps.InfoWindow();
 
-    // Google Map
     var map = new google.maps.Map(document.getElementById('map'), {
-      center: startingLocation,
-      zoom: 13
+      center: {
+        lat: 37.773972,
+        lng: -122.431297
+      },
+      zoom: 14
     });
 
     var addInfoWindow = function(marker, data) {
-      // Add a listener to the marker to open up an info window
       marker.addListener('click', function() {
         infoWindow.setContent(data);
         infoWindow.open(map, marker);
       });
     };
 
-    /** Public Functions **/
-    module.addMarker = function(location, data) {
-      // Create and add marker to the map
+    mapModule.addMarker = function(location, data) {
       var marker = new google.maps.Marker({position: location, map: map});
-
-      // Push marker to markers array
       markers.push(marker);
-
-      // Add info window
       addInfoWindow(marker, data);
     };
 
-    // Clears all markers from map
-    module.clearMarkers = function() {
-      // Must manuall set references to null to avoid memory leaks
+    mapModule.clearMarkers = function() {
       for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
       }
       markers = [];
     };
+    return mapModule;
+  }());
 
-    return module;
-  }(startingLocation));
-
+// UNderscore settings
   var setUnderscoreDefaultSettings = function() {
     _.templateSettings = {
       interpolate: /\{\{(.+?)\}\}/g, // print value: {{ value_name }}
@@ -69,66 +46,38 @@ $(document).ready(function() {
   }
   setUnderscoreDefaultSettings();
 
+  //Load template
+  var markerTemplate = _.template($("#markerDom").html());
+
   /** Autocomplete **/
   $("#search_input").autocomplete({
-    delay: 300, // Delay between keystrokes and fetch request
-    minLength: 3, // Minimum length of input for autocomplete
+    delay: 300,
+    minLength: 3,
     source: function(request, response) {
-      var type = $('.nav-pills .active').attr("value");
-
-      // Make call to server to get autocomplete results
       $.getJSON('/api/searches/autocomplete', {
         q: request.term,
-        type: type
+        type: 'movie'
       }, function(data, status, xhr) {
-
-        // Reformat the results to match the accepted signature of JQuery Autocomplete's Source
-        var mappedResults = data.map(function(d) {
-          d.label = (type == "movie")
-            ? d.title
-            : (d.director || d.name);
-          d.value = d.label;
-          return d;
+        var formattedResults = data.map(function(m) {
+          m.value = m.title;
+          return m;
         });
-
-        // Load jquery ui results
-        response(mappedResults);
+        response(formattedResults);
       });
     },
     select: function(event, ui) {
-      var type = $('.nav-pills .active').attr("value");
-
-      // Clear all map markers
       Map.clearMarkers();
-      // Get all movie locations by title then geocode their addresses to place on map
-      getLocations(type, ui.item);
+      getLocations('movie', ui.item);
     }
   });
 
-  /** Listeners **/
-
   $("#search_button").click(function() {
     var text = $("#search_input").val();
-    var type = $('.nav-pills .active').attr("value");
-
-    // Clear all map markers
     Map.clearMarkers();
-
-    // Get all movie locations by title then geocode their addresses to place on map
-    getLocations(type, null, text);
+    getLocations('movie', null, text);
   });
 
-  /** Helpers **/
-
-  /**
-		* Searches for all movie locations given an movie id, or director id
-		* param {string} endpoint - Server endpoint
-		* param {string} id - Movie or direcotr id
-		* param {string} tab - Current tab the search is under (ie. Title or Director)
-		* param {string} title - Search by movie titles, used for clicking search
-		*/
   var getLocations = function(type, movie, q) {
-    var markerTemplate = _.template($("#markerDom").html());
     if (movie) {
       //No exact match
       $.getJSON('api/movies/' + movie.id, {
@@ -137,35 +86,11 @@ $(document).ready(function() {
         }
       }, function(data) {
         console.log("Dataaa", data);
-        if (!data.length) {
-          // Show help information if no results
+        if (!data) {
           $(".help_container").css('visibility', 'visible');
         } else {
-          // Hide help information
           $(".help_container").css('visibility', 'hidden');
-          // Add all the movies location top the map
-          data.locations.forEach(function(d) {
-            if (d.lat && d.lon) {
-              var point = {
-                lat: parseFloat(d.lat),
-                lng: parseFloat(d.lon)
-              };
-              Map.addMarker(point, markerTemplate({
-                title: movie.title,
-                location: d.name,
-                releaseYear: movie.releaseYear,
-                director: movie.director
-                  ? movie.director.name
-                  : '',
-                actors: movie.actors
-                  ? movie.actors.join(",")
-                  : '',
-                productionCompany: movie.productionCompany
-                  ? movie.productionCompany
-                  : ''
-              }));
-            }
-          });
+          bindMarkerLocation(data);
         }
       });
     } else {
@@ -176,36 +101,12 @@ $(document).ready(function() {
       }, function(data) {
         console.log("Dataaa", data);
         if (!data.length) {
-          // Show help information if no results
           $(".help_container").css('visibility', 'visible');
         } else {
-          // Hide help information
           $(".help_container").css('visibility', 'hidden');
-          // Add all the movies location top the map
           data.forEach(function(movie) {
             if (movie.locations) {
-              movie.locations.forEach(function(d) {
-                if (d.lat && d.lon) {
-                  var point = {
-                    lat: parseFloat(d.lat),
-                    lng: parseFloat(d.lon)
-                  };
-                  Map.addMarker(point, markerTemplate({
-                    title: movie.title,
-                    location: d.name,
-                    releaseYear: movie.releaseYear,
-                    director: movie.director
-                      ? movie.director.name
-                      : '',
-                    actors: movie.actors
-                      ? movie.actors.join(",")
-                      : '',
-                    productionCompany: movie.productionCompany
-                      ? movie.productionCompany
-                      : ''
-                  }));
-                }
-              });
+              bindMarkerLocation(movie);
             }
           });
         }
@@ -213,4 +114,30 @@ $(document).ready(function() {
 
     }
   };
+
+  function bindMarkerLocation(movie) {
+    movie.locations.forEach(function(d) {
+      if (d.lat && d.lon) {
+        var point = {
+          lat: parseFloat(d.lat),
+          lng: parseFloat(d.lon)
+        };
+        Map.addMarker(point, markerTemplate({
+          title: movie.title,
+          location: d.name,
+          releaseYear: movie.releaseYear,
+          director: movie.director
+            ? movie.director.name
+            : '',
+          actors: movie.actors
+            ? movie.actors.join(",")
+            : '',
+          productionCompany: movie.productionCompany
+            ? movie.productionCompany
+            : ''
+        }));
+      }
+    });
+  }
+
 });
